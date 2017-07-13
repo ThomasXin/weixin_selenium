87be1b4c5bcf59353620cebdb1e9a22459eedc74
qingfengke <xihulu_job@163.com>
2017-07-14 00:22:45 +0800

    测试

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ from db import arcitle, keyword
 
 for i in keyword.find({}, {'keyword': '/1/i', 'md5_sign': 1}):
     # print(i['md5_sign'], i['keyword'])
-    for j in arcitle.find({'md5_sign' :str(i['md5_sign'])}):
+    for j in arcitle.find():
         print(j)
-        pass
