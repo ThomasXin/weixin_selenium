93172eca5f62386d7c725be37b1fe5f60ca1b1db
qingfengke <xihulu_job@163.com>
2017-07-15 01:03:18 +0800

    修改了提示信息

--- delete.py
+++ delete.py
@@ -5,8 +5,8 @@ def delete(strs):
     for i in keyword.find({'keyword': '%s' % strs}):
         arcitle.remove({'md5_sign': '%s' % i.get('md5_sign')})
         keyword.remove({'keyword': '%s' % strs})
-
+    print('已经移除---%s---相关信息' % strs)
 
 if __name__ == '__main__':
-    strs = raw_input('lalallalllalalla')
+    strs = raw_input('请输入要删除的关键词')
     delete(strs)
